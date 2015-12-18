43a9cd1dd1afe5db1cf17e5df1b52985d54b3ddb
obijuan <juan@iearobotics.com>
2015-12-18 08:47:07 +0100

    [UART-RX] rxleds_tb.v: translation into english. Code refactored

--- uart-rx/examples/rxleds_tb.v
+++ uart-rx/examples/rxleds_tb.v
@@ -21,6 +21,7 @@
 
 module rxleds_tb();
 
+//-- Baudrate for the simulation
 localparam BAUDRATE = `B115200;
 
 //-- clock tics needed for sending one serial package
@@ -29,36 +30,36 @@ localparam SERIAL_PACK = (BAUDRATE * 10);
 //-- Time between two characters
 localparam WAIT = (BAUDRATE * 4);
 
-
 //----------------------------------------
-//-- Tarea para enviar caracteres serie
+//-- Task for sending a character in serial
 //----------------------------------------
-  task send_car;
-    input [7:0] car;
+  task send_char;
+    input [7:0] char;
   begin
-    rx <= 0;                 //-- Bit start
-    #BAUDRATE rx <= car[0];   //-- Bit 0
-    #BAUDRATE rx <= car[1];   //-- Bit 1
-    #BAUDRATE rx <= car[2];   //-- Bit 2
-    #BAUDRATE rx <= car[3];   //-- Bit 3
-    #BAUDRATE rx <= car[4];   //-- Bit 4
-    #BAUDRATE rx <= car[5];   //-- Bit 5
-    #BAUDRATE rx <= car[6];   //-- Bit 6
-    #BAUDRATE rx <= car[7];   //-- Bit 7
-    #BAUDRATE rx <= 1;        //-- Bit stop
-    #BAUDRATE rx <= 1;        //-- Esperar a que se envie bit de stop
+    rx <= 0;                   //-- Send the Start bit
+    #BAUDRATE rx <= char[0];   //-- Bit 0
+    #BAUDRATE rx <= char[1];   //-- Bit 1
+    #BAUDRATE rx <= char[2];   //-- Bit 2
+    #BAUDRATE rx <= char[3];   //-- Bit 3
+    #BAUDRATE rx <= char[4];   //-- Bit 4
+    #BAUDRATE rx <= char[5];   //-- Bit 5
+    #BAUDRATE rx <= char[6];   //-- Bit 6
+    #BAUDRATE rx <= char[7];   //-- Bit 7
+    #BAUDRATE rx <= 1;         //-- stop bit
+    #BAUDRATE rx <= 1;         //-- Wait until the bits stop is sent
   end
   endtask
 
-
-//-- Registro para generar la señal de reloj
+//-- System clock
 reg clk = 0;
 
-//-- Cables para las pruebas
+//-- Wire connected to the rx port for transmiting to the receiver
 reg rx = 1;
+
+//-- For connecting the leds
 wire [3:0] leds;
 
-//-- Instanciar el modulo rxleds
+//-- Instantiate the entity to test
 rxleds #(.BAUDRATE(BAUDRATE))
   dut(
     .clk(clk),
@@ -70,18 +71,17 @@ rxleds #(.BAUDRATE(BAUDRATE))
 always
   # 0.5 clk <= ~clk;
 
-//-- Proceso al inicio
 initial begin
 
-  //-- Fichero donde almacenar los resultados
+  //-- File where to store the simulation
   $dumpfile("examples/rxleds_tb.vcd");
   $dumpvars(0, rxleds_tb);
 
-  //-- Enviar datos de prueba
-  #BAUDRATE    send_car(8'h55);
-  #WAIT send_car("K");
+  //-- Sent some data
+  #BAUDRATE    send_char(8'h55);
+  #WAIT        send_char("K");
 
-  #(WAIT*4) $display("END of the simulation");
+  #(WAIT * 4) $display("END of the simulation");
   $finish;
 end
 
