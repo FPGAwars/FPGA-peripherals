1de1f26af193996fda3da88378b85d020aedbec3
obijuan <juan@iearobotics.com>
2016-03-13 21:04:35 +0100

    Servo unit is now in the ServoUnit.v file

--- servos/test/src/test.v
+++ servos/test/src/test.v
@@ -6,35 +6,16 @@ module test(input wire clk,
             output wire led0);
 
 //-- Just for debugging
-assign led0 = 1'b1;
+assign led0 = 1;
 
-localparam M = 47;
-localparam N = $clog2(M);
+reg [7:0] pos = 8'h80;
 
-reg [N-1:0] divcounter = 0;
-reg tic = 0;
+//-- Intantiate the servo unit
+ServoUnit
+  SERVO0 (.clk(clk),
+          .pos(pos),
+          .servo(clk_out));
 
-//-- Ticks generation. A pulse is emmited every M system clock cycles
-always @(posedge clk)
-  tic <= (divcounter == M - 2);
-
-//-- Module M counter
-always @(posedge clk)
-  if (tic)
-    divcounter <= 0;
-  else
-    divcounter <= divcounter + 1;
-
-//-- Angle counter
-reg [11:0] angle_counter = 0;
-reg [7:0] servo_pos = 250;
-
-always @(posedge clk)
-  if (tic)
-    angle_counter <= angle_counter + 1;
-
-always @(posedge clk)
-  clk_out <= (angle_counter < {4'b0001, servo_pos});
 
 
 endmodule
