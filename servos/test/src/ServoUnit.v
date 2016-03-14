37375825b84be242c45bdfcd6df0ee18299985c8
obijuan <juan@iearobotics.com>
2016-03-14 09:52:18 +0100

    Servounit: works ok with futaba 3003 servos

--- servos/test/src/ServoUnit.v
+++ servos/test/src/ServoUnit.v
@@ -3,9 +3,9 @@
 //-- Servo test
 module ServoUnit(input wire clk,         //-- System clock
                  input wire [7:0] pos,   //-- Servo pos 0 - 255
-                 output reg servo);     //-- Servo control signal
+                 output reg servo);      //-- Servo control signal
 
-localparam M = 47;
+localparam M = 93; //-- 94
 localparam N = $clog2(M);
 
 reg [N-1:0] divcounter = 0;
@@ -23,14 +23,16 @@ always @(posedge clk)
    divcounter <= divcounter + 1;
 
 //-- Angle counter
-reg [11:0] angle_counter = 0;
+reg [10:0] angle_counter = 0;
+
+wire [8:0] pose = {1'b0, pos} + 9'd46;
 
 always @(posedge clk)
  if (tic)
    angle_counter <= angle_counter + 1;
 
 always @(posedge clk)
- servo <= (angle_counter < {4'b0001, pos});
+ servo <= (angle_counter < {3'b00, pose});
 
 
 endmodule
